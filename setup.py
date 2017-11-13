f68a6d04a16c1c321fc568fca62bdb478a993985
Ryan Francis <rfrancis@graphicproducts.local>
2017-11-13 12:03:44 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='the_beyond',
+      version='0.01',
+      description='Data Processing Tools for Machine Learning and Data Analysis.',
+      url='https://github.com/2yan/the_beyond',
+      author='Ryan Francis',
+      author_email='2yan@outlook.com',
+      license='MIT',
+      packages=['the_beyond'],
+      install_requires= ['numpy'],
+      zip_safe=True)
