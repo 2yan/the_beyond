dbe5fa328c240cf8b7519ab8b693da433e885300
Ryan Francis <rfrancis@graphicproducts.local>
2017-11-13 11:55:34 -0800

    seed generation

--- the_beyond/WordsWorth.py
+++ the_beyond/WordsWorth.py
@@ -4,18 +4,23 @@ import numpy as np
 class WordsWorth():
     text = None
     one_hot = None
-    char_to_id = {}
-    char_to_one_hot = {}
-    id_to_char = {}
-    id_to_one_hot = {}
+    char_to_id = None
+    char_to_one_hot = None
+    id_to_char = None
+    id_to_one_hot = None
 
 
     def __init__(s, text):
         'Generates a new WordsWorth Object'
         s.text = text
+        s.id_to_one_hot = {}
+        s.char_to_one_hot = {}
+
         unique = set(text)
         s.id_to_char = dict(zip(range(0, len(unique)), unique))
         s.char_to_id = dict(zip(s.id_to_char.values(), s.id_to_char.keys()))
+
+
 
         for key, char in s.id_to_char.items():
             one_hot = [0] * len(unique)
@@ -38,6 +43,14 @@ class WordsWorth():
             y.append(s.one_hot[num + letters])
 
         return np.array(X), np.array(y)
+
+    def generate_seed(s, seed_text, letters):
+
+        final = []
+        for letter in seed_text:
+            final.append(s.char_to_one_hot[letter])
+        final = [final]
+        return np.array(final)
 
     def __pred_to_text(s, result):
         return s.id_to_char[list(result).index(result.max())]
